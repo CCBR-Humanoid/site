b1130496811f19b054ba901167224a9c37353e24
ryanrudes <ryanrudes@gmail.com>
2025-08-23 13:39:53 -0400

    Add GithubButton component for linking to GitHub organization

---
+++ app/components/GithubButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <UButton
+    color="neutral"
+    icon="i-simple-icons-github"
+    size="xl"
+    target="_blank"
+    :to="appConfig.social?.githubOrgUrl"
+    variant="subtle"
+  >
+    Repositories
+  </UButton>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
