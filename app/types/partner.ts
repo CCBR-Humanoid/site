0fefc275c4dd1ed65913fb98e1920d3d1ae53dc1
ryanrudes <ryanrudes@gmail.com>
2025-08-26 14:52:31 -0400

    types: add Partner, TeamMember, SocialLink types and index barrel

---
+++ app/types/partner.ts
@@ -0,0 +1,6 @@
+export interface Partner {
+  title: string
+  subtitle?: string
+  logo: string
+  url: string
+}
---
+++ app/types/team-member.ts
@@ -0,0 +1,9 @@
+import type { SocialLink } from './social-link'
+
+export interface TeamMember {
+  name: string;
+  role: string;
+  avatar: string;
+  profileUrl: string;
+  socials: SocialLink[];
+}
