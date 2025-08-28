89ea5f1e2c8edccac991c5cb265f2ec4414d27e1
ryanrudes <ryanrudes@gmail.com>
2025-08-28 15:22:15 -0400

    chore: point internal repo links to CCBR-Humanoid/site

--- app/constants.ts
+++ app/constants.ts
@@ -3,5 +3,5 @@
 export const TEAM_NAME = 'CCBR'
 export const GITHUB_ORG_URL = 'https://github.com/CCBR-Humanoid'
 // URL of this documentation site's GitHub repository
-export const GITHUB_REPO_URL = 'https://github.com/willcforte/ccbr-docs'
+export const GITHUB_REPO_URL = 'https://github.com/CCBR-Humanoid/site'
 export const LINKEDIN_URL = 'https://www.linkedin.com/company/ccbr-robotics'
